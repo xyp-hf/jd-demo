7edd3dd4f46b7f00d7aa25d7dc0783bb2b995330
徐玉朋 <2766588380@qq.com>
2023-10-25 00:20:45 +0800

    [路由参数的传递]

--- jingdong/src/views/home/StaticPart.vue
+++ jingdong/src/views/home/StaticPart.vue
@@ -76,8 +76,8 @@ export default {
 .search {
   margin-bottom: .12rem;
   line-height: .32rem;
-  background: #F5F5F5;
-  color: #B7B7B7;
+  background: $search-bgColor;
+  color: $search-fontColor;
   border-radius: .16rem;
   .iconfont {
     display: inline-block;
